40b28aa7a4aae872e486d86c4f57a96fdfa897f0
todanh <todanh2706@gmail.com>
2025-09-30 21:30:18 +0700

    debug reload

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(session({
     secret: process.env.SESSION_SECRET || "supersecret",
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
         secure: true,
         httpOnly: true,
